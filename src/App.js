20c05788456265263aa3149ed6eece38aa24ef02
MarcosKern <vkg.marcos@gmail.com>
2022-08-24 22:43:53 -0300

    sexto requisito - ok

--- src/App.js
+++ src/App.js
@@ -1,13 +1,149 @@
 import React from 'react';
 import Form from './components/Form';
+import Card from './components/Card';
 
 class App extends React.Component {
+  state = {
+    cardName: '',
+    cardDescription: '',
+    attr1: '0',
+    attr2: '0',
+    attr3: '0',
+    rariti: '',
+    cardImage: '',
+    isTrunfo: false,
+    isSaveButtonDisabled: true,
+    maxAttr: 210,
+    deck: [],
+  };
+
+  handleEvent = async (event) => {
+    const {
+      target: { name, value, checked, type },
+    } = event;
+    if (type === 'checkbox') {
+      this.setState({ [name]: checked });
+    } else {
+      this.setState({ [name]: value }, () => {
+        const {
+          cardName,
+          cardDescription,
+          cardImage,
+          rariti,
+          attr1,
+          attr2,
+          attr3,
+          maxAttr,
+        } = this.state;
+        const isEmpty = [
+          cardName,
+          cardDescription,
+          cardImage,
+          rariti,
+        ].some((cardSession) => cardSession === '');
+        const max = 90;
+        const min = 0;
+        const atribute1 = Number(attr1);
+        const atribute2 = Number(attr2);
+        const atribute3 = Number(attr3);
+        const sum = atribute1 + atribute2 + atribute3;
+        if (
+          atribute1 <= max
+          && atribute1 >= min
+          && atribute2 <= max
+          && atribute2 >= min
+          && atribute3 <= max
+          && atribute3 >= min
+          && sum <= maxAttr
+          && isEmpty === false
+        ) {
+          this.setState({ isSaveButtonDisabled: false });
+        } else {
+          this.setState({ isSaveButtonDisabled: true });
+        }
+      });
+    }
+  };
+
+  saveButton = (event) => {
+    event.preventDefault();
+    const {
+      cardName,
+      cardDescription,
+      cardImage,
+      rariti,
+      attr1,
+      attr2,
+      attr3,
+      isTrunfo,
+      deck,
+    } = this.state;
+    deck.push(
+      {
+        nome: cardName,
+        descricao: cardDescription,
+        imagem: cardImage,
+        atributo1: attr1,
+        atributo2: attr2,
+        atributo3: attr3,
+        raridade: rariti,
+        trunfo: isTrunfo,
+      },
+    );
+    this.setState({
+      cardName: '',
+      cardDescription: '',
+      attr1: '0',
+      attr2: '0',
+      attr3: '0',
+      rariti: '',
+      cardImage: '',
+    });
+  };
+
   render() {
+    const {
+      cardName,
+      cardDescription,
+      attr1,
+      attr2,
+      attr3,
+      rariti,
+      cardImage,
+      isTrunfo,
+      isSaveButtonDisabled,
+    } = this.state;
+
     return (
       <div>
         <h1>Tryunfo</h1>
         <section>
-          <Form />
+          <Form
+            cardName={ cardName }
+            cardDescription={ cardDescription }
+            cardAttr1={ attr1 }
+            cardAttr2={ attr2 }
+            cardAttr3={ attr3 }
+            cardImage={ cardImage }
+            cardRare={ rariti }
+            cardTrunfo={ isTrunfo }
+            hasTrunfo=""
+            isSaveButtonDisabled={ isSaveButtonDisabled }
+            onInputChange={ this.handleEvent }
+            onSaveButtonClick={ this.saveButton }
+          />
+          <div>
+            <Card
+              cardName={ cardName }
+              cardDescription={ cardDescription }
+              cardAttr1={ attr1 }
+              cardAttr2={ attr2 }
+              cardAttr3={ attr3 }
+              cardImage={ cardImage }
+              cardRare={ rariti }
+              cardTrunfo={ isTrunfo }
+            />
+          </div>
         </section>
       </div>
     );
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,59 +1,100 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Form extends React.Component {
   render() {
+    const {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      cardTrunfo,
+      isSaveButtonDisabled,
+      onInputChange,
+      onSaveButtonClick,
+    } = this.props;
     return (
       <form action="">
         <input
           type="text"
-          name="card-name"
+          name="cardName"
           id=""
           data-testid="name-input"
+          value={ cardName }
+          onChange={ onInputChange }
         />
         <textarea
-          name=""
+          name="cardDescription"
           id=""
           cols="30"
           rows="10"
           data-testid="description-input"
+          value={ cardDescription }
+          onChange={ onInputChange }
         />
         <input
           type="number"
-          name=""
+          name="attr1"
+          min="0"
+          max="90"
           id=""
           data-testid="attr1-input"
+          value={ cardAttr1 }
+          onChange={ onInputChange }
         />
         <input
           type="number"
-          name=""
+          name="attr2"
+          min="0"
+          max="90"
           id=""
           data-testid="attr2-input"
+          value={ cardAttr2 }
+          onChange={ onInputChange }
         />
         <input
           type="number"
-          name=""
+          name="attr3"
+          min="0"
+          max="90"
           id=""
           data-testid="attr3-input"
+          value={ cardAttr3 }
+          onChange={ onInputChange }
         />
         <input
+          name="cardImage"
           type="text"
           data-testid="image-input"
+          value={ cardImage }
+          onChange={ onInputChange }
         />
         <select
-          name=""
+          name="rariti"
           id=""
           data-testid="rare-input"
+          value={ cardRare }
+          onChange={ onInputChange }
         >
-          <option value="normal">Normal</option>
-          <option value="raro">Rara</option>
-          <option value="muito raro">Muito rara</option>
+          <option value="normal">normal</option>
+          <option value="raro">raro</option>
+          <option value="muito raro">muito raro</option>
         </select>
-        <checkbox
+        <input
+          name="isTrunfo"
+          type="checkbox"
           data-testid="trunfo-input"
+          checked={ cardTrunfo }
+          onChange={ onInputChange }
         />
         <button
           type="submit"
           data-testid="save-button"
+          disabled={ isSaveButtonDisabled }
+          onClick={ onSaveButtonClick }
         >
           Salvar
         </button>
@@ -61,5 +102,20 @@ class Form extends React.Component {
     );
   }
 }
+
+Form.propTypes = {
+  cardName: PropTypes.string.isRequired,
+  cardDescription: PropTypes.string.isRequired,
+  cardAttr1: PropTypes.string.isRequired,
+  cardAttr2: PropTypes.string.isRequired,
+  cardAttr3: PropTypes.string.isRequired,
+  cardImage: PropTypes.string.isRequired,
+  cardRare: PropTypes.string.isRequired,
+  cardTrunfo: PropTypes.bool.isRequired,
+  hasTrunfo: PropTypes.bool.isRequired,
+  isSaveButtonDisabled: PropTypes.bool.isRequired,
+  onInputChange: PropTypes.func.isRequired,
+  onSaveButtonClick: PropTypes.func.isRequired,
+};
 
 export default Form;
